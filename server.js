8c7ed7efadf64bee9f25805dbdced1f82b222ffe
meztinosdunken <orlanmorry5462@hotmail.com>
2025-08-28 20:52:45 -0500

    Improve MongoDB connection handling and add comments

--- server.js
+++ server.js
@@ -11,13 +11,14 @@ app.use(express.json());
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/sprint-backlog', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected!'))
-  .catch(err => console.log(err));
+  .catch(err => console.error('MongoDB connection error:', err)); // Improved error logging
 
-// Sample route
+// Sample route to test API
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
